0bc850cbee7c97924a1886234edbd1eb67a37c59
Purdea Andrei <andrei@purdea.ro>
2021-04-25 15:27:23 +0300

    haptic: Feature to disable it when usb port is not configured or suspended.
    This also add support for specifying a LED pin to indicate haptic status,
    and also adds support for a haptic-enable pin, which is useful to turn off
    the boost converter on the solenoid driver.

--- quantum/power.c
+++ quantum/power.c
@@ -16,6 +16,9 @@
  */
 
 #include "power.h"
+#if defined(HAPTIC_ENABLE)
+#    include "haptic.h"
+#endif
 
 enum power_state power_state;
 
@@ -24,6 +27,9 @@ __attribute__((weak)) void notify_power_state_change_kb(void) {
 
 static void notify_power_state_change(void)
 {
+#if defined(HAPTIC_ENABLE) && HAPTIC_OFF_IN_LOW_POWER
+    haptic_notify_power_state_change();
+#endif
     notify_power_state_change_kb();
 }
 
